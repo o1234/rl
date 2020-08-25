a8bae4bd4ac371820da9422435183aa3596a63c7
o1234 <893502497@qq.com>
2020-08-25 19:03:30 +0800

    Update run_this.py
    tf 1.0 -> tf 2.0 (to do)

--- run_this.py
+++ run_this.py
@@ -1,5 +1,15 @@
 from maze_env import Maze
-from RL_brain import DeepQNetwork
+#from RL_brain import DeepQNetwork
+import wandb
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.optimizers import Adam
+
+import gym
+import argparse
+import numpy as np
+from collections import deque
+import random
 
 
 def run_maze():
@@ -13,15 +23,18 @@ def run_maze():
             env.render()
 
             # RL choose action based on observation
-            action = RL.choose_action(observation)
+            #action = RL.choose_action(observation)
+            action = agent.model.get_action(observation)
 
             # RL take action and get next observation and reward
             observation_, reward, done = env.step(action)
 
-            RL.store_transition(observation, action, reward, observation_)
+            #RL.store_transition(observation, action, reward, observation_)
+            agent.buffer.put(observation, action, reward, observation_, done)
 
             if (step > 200) and (step % 5 == 0):
-                RL.learn()
+                #RL.learn()
+                agent.replay()
 
             # swap observation
             observation = observation_
@@ -36,17 +49,108 @@ def run_maze():
     env.destroy()
 
 
+class ReplayBuffer:
+    def __init__(self, capacity=10000):
+        self.buffer = deque(maxlen=capacity)
+
+    def put(self, state, action, reward, next_state, done):
+        self.buffer.append([state, action, reward, next_state, done])
+
+    def sample(self):
+        sample = random.sample(self.buffer, args.batch_size)
+        states, actions, rewards, next_states, done = map(np.asarray, zip(*sample))
+        states = np.array(states).reshape(args.batch_size, -1)
+        next_states = np.array(next_states).reshape(args.batch_size, -1)
+        return states, actions, rewards, next_states, done
+
+    def size(self):
+        return len(self.buffer)
+
+class ActionStateModel:
+    def __init__(self, state_dim, aciton_dim):
+        self.state_dim = state_dim
+        self.action_dim = aciton_dim
+        self.epsilon = args.eps
+
+        self.model = self.create_model()
+
+    def create_model(self):
+        model = tf.keras.Sequential([
+            Input((self.state_dim,)),
+            Dense(32, activation='relu'),
+            Dense(16, activation='relu'),
+            Dense(self.action_dim)
+        ])
+        model.compile(loss='mse', optimizer=Adam(args.lr))
+        return model
+
+    def predict(self, state):
+        return self.model.predict(state)
+
+    def get_action(self, state):
+        state = np.reshape(state, [1, self.state_dim])
+        self.epsilon *= args.eps_decay
+        self.epsilon = max(self.epsilon, args.eps_min)
+        q_value = self.predict(state)[0]
+        if np.random.random() < self.epsilon:
+            return random.randint(0, self.action_dim - 1)
+        return np.argmax(q_value)
+
+    def train(self, states, targets):
+        self.model.fit(states, targets, epochs=1, verbose=0)
+
+class Agent:
+    def __init__(self, env):
+        self.env = env
+        self.state_dim = self.env.observation_space.shape[0]
+        self.action_dim = self.env.action_space.n
+
+        self.model = ActionStateModel(self.state_dim, self.action_dim)
+        self.target_model = ActionStateModel(self.state_dim, self.action_dim)
+        self.target_update()
+
+        self.buffer = ReplayBuffer()
+
+    def target_update(self):
+        weights = self.model.model.get_weights()
+        self.target_model.model.set_weights(weights)
+
+    def replay(self):
+        for _ in range(10):
+            states, actions, rewards, next_states, done = self.buffer.sample()
+            targets = self.target_model.predict(states)
+            next_q_values = self.target_model.predict(next_states).max(axis=1)
+            targets[range(args.batch_size), actions] = rewards + (1 - done) * next_q_values * args.gamma
+            self.model.train(states, targets)
+
+    def train(self, max_episodes=1000):
+        for ep in range(max_episodes):
+            done, total_reward = False, 0
+            state = self.env.reset()
+            while not done:
+                action = self.model.get_action(state)
+                next_state, reward, done, _ = self.env.step(action)
+                self.buffer.put(state, action, reward * 0.01, next_state, done)
+                total_reward += reward
+                state = next_state
+            if self.buffer.size() >= args.batch_size:
+                self.replay()
+            self.target_update()
+            print('EP{} EpisodeReward={}'.format(ep, total_reward))
+            wandb.log({'Reward': total_reward})
+
 if __name__ == "__main__":
     # maze game
     env = Maze()
-    RL = DeepQNetwork(env.n_actions, env.n_features,
-                      learning_rate=0.01,
-                      reward_decay=0.9,
-                      e_greedy=0.9,
-                      replace_target_iter=200,
-                      memory_size=2000,
-                      # output_graph=True
-                      )
+    # RL = DeepQNetwork(env.n_actions, env.n_features,
+    #                   learning_rate=0.01,
+    #                   reward_decay=0.9,
+    #                   e_greedy=0.9,
+    #                   replace_target_iter=200,
+    #                   memory_size=2000,
+    #                   # output_graph=True
+    #                   )
+    agent = Agent(env)
     env.after(100, run_maze)
     env.mainloop()
-    RL.plot_cost()
+    #RL.plot_cost()
